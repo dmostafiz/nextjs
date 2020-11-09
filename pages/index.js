d4f04fde060f74e14e001f5248fbe90cc693c105
Mostafiz Rahaman <62952421+dmostafiz@users.noreply.github.com>
2020-11-10 01:40:50 +0600

    new update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://dcodea.com">Dcodea Technologies!</a>
+          Welcome to <br/> <a href="https://dcodea.com">Dcodea Software Technologies!</a>
         </h1>
 
         <p className={styles.description}>
